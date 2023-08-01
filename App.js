fdd4cb17573c0674727f88013b17f1626ca89ed7
Chelsea <chelsea@tripwiretech.com>
2023-08-01 16:07:31 +1200

    Tutorial

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View, Text } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useState } from "react";
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (e, selectedDate) => {
+    setDate(selectedDate);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <DateTimePicker
+        value={date}
+        mode={"date"}
+        is24Hour={true}
+        onChange={onChange}
+      />
+      <DateTimePicker
+        value={date}
+        mode={"time"}
+        is24Hour={true}
+        onChange={onChange}
+      />
+      <Text>{date.toLocaleString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
